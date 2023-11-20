8734f32545765580a13e03eacc14adb285b0411b
EdebaliErman <edebalierman@gmail.com>
2023-11-20 21:30:33 +0300

    Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,9 @@ import "./Header.css"
 function Header() {
   return (
     <header>
-      
+      <h1>Reliable, efficient delivery</h1>
+      <h2>Powered by Technology</h2>
+      <h4>Our Artificial Intelligence powered tools use millions of profect data points to ensure that your project is succcesful</h4>
     </header>
   )
 }
